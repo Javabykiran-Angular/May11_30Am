dd324e66c4531b701dbd627e3d3c871f56c96886
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-05-11 12:36:52 +0530

    Day4

---
+++ Typescript/Day4/demo1.ts
@@ -0,0 +1,65 @@
+
+//loop
+// for loop,while loop,d-while loop
+//foreach loop
+//break,continue,
+// if , if-else,nested if-else
+// switch()
+
+// if(21<4){
+//     console.log("Condition is true..");
+// }else {
+//     console.log("Condition is false..");
+// }
+
+// var count=5;
+// while(count!=0){
+// // console.log('Count is '+count);
+// count--;
+// }
+
+// var count1=5;
+// do{
+
+// }while(count!=0);
+
+
+//for loop
+//
+for(var i=0;i<5;i++){
+    // console.log(`
+    // Value of i ${i} -->
+    // `);
+// console.log("Value of i is "+i+"=>");
+}
+//var => has its scope throught out the program=>
+// It has Global Scope
+// console.log('Value of i after loop '+i);
+
+//let => has scope within nearest block
+
+
+for (let i1 = 0; i1 <5; i1++) {
+//   console.log("Value of i is "+i1);    
+}
+// console.log("Value of i After loop "+i1);
+
+// const => it has global scope
+
+const pi=3.14;
+//  pi=5;
+
+// var, let , const 
+
+//Switch
+var choice:number=12;
+switch(choice){
+
+    case 1://logic
+            console.log('U are in case 1..')
+            break;
+    case 2: console.log("U are in case 2..");
+            break;
+    default: console.log("U are in default case..");
+            break;
+}
